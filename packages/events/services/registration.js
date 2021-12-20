834e90bca90145c811c69b1a49965aceee44fa1e
srosset81 <srosset81@gmail.com>
2021-12-20 11:19:19 +0100

    Share profile with organizer after join event + fix leave event

--- packages/events/services/registration.js
+++ packages/events/services/registration.js
@@ -1,7 +1,5 @@
 const { MoleculerError } = require('moleculer').Errors;
-const { defaultToArray } = require('@semapps/ldp');
 const { ActivitiesHandlerMixin, OBJECT_TYPES } = require('@semapps/activitypub');
-const { MIME_TYPES } = require('@semapps/mime-types');
 const { JOIN_EVENT, LEAVE_EVENT } = require('../patterns');
 
 module.exports = {
@@ -21,6 +19,35 @@ module.exports = {
   activities: {
     joinEvent: {
       match: JOIN_EVENT,
+      async onEmit(ctx, activity, emitterUri) {
+        const emitter = await ctx.call('activitypub.actor.get', { actorUri: emitterUri, webId: 'system' });
+
+        // If the emitter has a profile, it means the contacts app is activated
+        if( emitter.url ) {
+          const organizerUri = activity.object['dc:creator'];
+
+          const rights = await broker.call('webacl.resource.hasRights', {
+            resourceUri: emitter.url,
+            rights: { read: true },
+            webId: organizerUri,
+          })
+
+          // If the organizer cannot view my profile, give him the right
+          // TODO if the organizer is added later through the contacts app, remove this right
+          if( rights && rights.read !== true ) {
+            await ctx.call('webacl.resource.addRights', {
+              resourceUri: emitter.url,
+              additionalRights: {
+                user: {
+                  uri: organizerUri,
+                  read: true,
+                },
+              },
+              webId: emitter.id,
+            });
+          }
+        }
+      },
       async onReceive(ctx, activity) {
         const event = activity.object;
 
@@ -61,7 +88,7 @@ module.exports = {
         if (
           !(await ctx.call('activitypub.collection.includes', {
             collectionUri: event['apods:attendees'],
-            item: activity.actor,
+            itemUri: activity.actor,
           }))
         ) {
           throw new MoleculerError('You are not attending this event', 400);

e9235708fc029eebe22640e7f0d2ea7ac9eb117c
srosset81 <srosset81@gmail.com>
2022-04-29 13:20:58 +0200

    Config mail catcher with tests so it's easy to see the result

--- tests/config.js
+++ tests/config.js
@@ -7,4 +7,11 @@ module.exports = {
   SPARQL_ENDPOINT: process.env.SEMAPPS_SPARQL_ENDPOINT,
   JENA_USER: process.env.SEMAPPS_JENA_USER,
   JENA_PASSWORD: process.env.SEMAPPS_JENA_PASSWORD,
+  FROM_EMAIL: process.env.SEMAPPS_FROM_EMAIL,
+  FROM_NAME: process.env.SEMAPPS_FROM_NAME,
+  SMTP_HOST: process.env.SEMAPPS_SMTP_HOST,
+  SMTP_PORT: parseInt(process.env.SEMAPPS_SMTP_PORT, 10),
+  SMTP_SECURE: process.env.SEMAPPS_SMTP_SECURE === 'true',
+  SMTP_USER: process.env.SEMAPPS_SMTP_USER,
+  SMTP_PASS: process.env.SEMAPPS_SMTP_PASS,
 };

9227525e888690cc795fe9f975a18c195ee2930d
srosset81 <srosset81@gmail.com>
2022-12-29 11:30:39 +0100

    Initial commit

---
+++ website/src/config.mjs
@@ -0,0 +1,38 @@
+export const SITE = {
+	name: 'ActivityPods',
+
+	origin: 'https://activitypods.org',
+	basePathname: '/',
+	trailingSlash: false,
+
+	title: 'ActivityPods - PODs powered with ActivityPub',
+	description: '🚀 AstroWind is a free and ready to start template to make your website using Astro and Tailwind CSS.',
+
+	googleAnalyticsId: false, // or "G-XXXXXXXXXX",
+	googleSiteVerificationId: 'orcPxI47GSa-cRvY11tUe6iGg2IO_RPvnA1q95iEM3M',
+};
+
+export const BLOG = {
+	disabled: false,
+	postsPerPage: 4,
+
+	blog: {
+		disabled: false,
+		pathname: 'blog', // blog main path, you can change this to "articles" (/articles)
+	},
+
+	post: {
+		disabled: false,
+		pathname: '', // empty for /some-post, value for /pathname/some-post
+	},
+
+	category: {
+		disabled: false,
+		pathname: 'category', // set empty to change from /category/some-category to /some-category
+	},
+
+	tag: {
+		disabled: false,
+		pathname: 'tag', // set empty to change from /tag/some-tag to /some-tag
+	},
+};
---
+++ website/tailwind.config.cjs
@@ -0,0 +1,48 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
+	theme: {
+		// colors: {
+		// 	violet: '#7c4dff',
+		// 	pink: '#f1007e'
+		// },
+		extend: {
+			colors: {
+				primary: colors.purple,
+				secondary: colors.pink,
+			},
+			fontFamily: {
+				sans: ["'InterVariable'", ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+	plugins: [require('@tailwindcss/typography')],
+	darkMode: 'class',
+};
+
+/* 
+
+  Alternative tailwind.config.js
+  
+  NOTE: Add this fonts to <head>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;700&display=swap" rel="stylesheet" />
+*/
+
+// module.exports = {
+//   content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
+//   theme: {
+//     extend: {
+//       colors: {
+//         primary: colors.cyan,
+//         secondary: colors.lime,
+//       },
+//       fontFamily: {
+//         sans: ["'Nunito'", ...defaultTheme.fontFamily.sans],
+//       },
+//     },
+//   },
+//   plugins: [require("@tailwindcss/typography")],
+//   darkMode: "class",
+// };

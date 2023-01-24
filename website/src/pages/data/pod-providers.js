f1c45cf8cfaa95332d49bb37620bd5e343b70a7d
srosset81 <srosset81@gmail.com>
2023-01-24 20:36:40 +0100

    Fix endpoints

--- website/src/pages/data/pod-providers.js
+++ website/src/pages/data/pod-providers.js
@@ -2,11 +2,15 @@ import ldpContainer from "~/utils/ldpContainer";
 import podProviders from "~/config/pod-providers";
 
 export async function get({params, request}) {
-    return {
-        body: JSON.stringify(ldpContainer(
-            request.url, 
-            podProviders, 
-            "apods:domainName"
-        )),
-    };
+	return new Response(
+		JSON.stringify(ldpContainer(
+			request.url,
+			podProviders,
+			"apods:domainName"
+		)), 
+		{
+			headers: {
+				'Content-Type': 'application/ld+json'
+		}
+	});
 }
--- website/src/pages/data/trusted-apps.js
+++ website/src/pages/data/trusted-apps.js
@@ -2,11 +2,15 @@ import ldpContainer from "~/utils/ldpContainer";
 import trustedApps from "~/config/trusted-apps";
 
 export async function get({params, request}) {
-    return {
-        body: JSON.stringify(ldpContainer(
-            request.url, 
-            trustedApps, 
-            "apods:domainName"
-        )),
-    };
+	return new Response(
+		JSON.stringify(ldpContainer(
+			request.url,
+			trustedApps,
+			"apods:domainName"
+		)),
+		{
+			headers: {
+				'Content-Type': 'application/ld+json'
+			}
+		});
 }

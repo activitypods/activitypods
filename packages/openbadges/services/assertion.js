dc7814f620c304b3569422e3ad5431c5c2207e98
srosset81 <srosset81@gmail.com>
2023-02-06 12:16:43 +0100

    Improvements

--- packages/openbadges/services/assertion.js
+++ packages/openbadges/services/assertion.js
@@ -28,71 +28,18 @@ module.exports = {
   hooks: {
     after: {
       async create(ctx, res) {
-        const { resourceUri: assertionUri, newData: assertion, webId } = res;
+        const { resourceUri, newData: assertion, webId } = res;
 
-        const badge = await ctx.call('ldp.resource.get', {
-          resourceUri: assertion.badge,
-          accept: MIME_TYPES.JSON,
+        const bakedBadgeUri = await ctx.call('openbadges.baked-badge.bake', {
+          assertion,
           webId
-        });
-
-        const badgeImage = await ctx.call('ldp.resource.get', {
-          resourceUri: badge['schema:image'],
-          forceSemantic: true,
-          accept: MIME_TYPES.JSON,
-          webId
-        });
-
-        const bakedBadgeContainerUri = urlJoin(webId, 'data', 'baked-badges');
-
-        const bakedBadgeUri = await ctx.call('ldp.resource.generateId', {
-          containerUri: bakedBadgeContainerUri
         })
 
-        const framedAssertion = await ctx.call('jsonld.frame', {
-          input: {
-            ...assertion,
-            'schema:image': {
-              '@id': bakedBadgeUri
-            }
-          },
-          frame: {
-            '@context': 'https://openbadgespec.org/v2/context.json',
-            '@id': assertionUri
-          }
-        });
-
-        const buffer = await sharp('./' + badgeImage['semapps:localPath']).png().toBuffer();
-        const readableStream = Readable.from(buffer);
-        const bakedFileStream = await readableStream.pipe(pngitxt.set({ keyword: 'openbadge', value: JSON.stringify(framedAssertion)} ));
-
-        // TODO see why we can't use ldp.container.post
-
-        const bakedBadge = await ctx.call('ldp.resource.upload', {
-          resourceUri: bakedBadgeUri,
-          file: {
-            encoding: badgeImage['semapps:encoding'],
-            mimetype: badgeImage['semapps:mimeType'],
-            readableStream: bakedFileStream,
-          }
-        });
-
-        await ctx.call('triplestore.insert', {
-          resource: `<${bakedBadgeContainerUri}> <http://www.w3.org/ns/ldp#contains> <${bakedBadgeUri}>`,
-          webId
-        });
-
-        await ctx.call('ldp.resource.create', {
-          resource: bakedBadge,
-          contentType: MIME_TYPES.JSON,
-          webId
-        });
-
         await ctx.call('ldp.resource.patch', {
-          resourceUri: assertionUri,
+          resourceUri: resourceUri,
           triplesToAdd: [
             triple(
-              namedNode(assertionUri),
+              namedNode(resourceUri),
               namedNode('http://schema.org/image'),
               namedNode(bakedBadgeUri)
             )
@@ -101,13 +48,13 @@ module.exports = {
         });
 
         const updatedAssertion = await ctx.call('ldp.resource.get', {
-          resourceUri: assertionUri,
+          resourceUri,
           accept: MIME_TYPES.JSON,
           webId
         });
 
         return {
-          resourceUri: assertionUri,
+          resourceUri,
           newData: updatedAssertion,
           webId
         }
--- packages/openbadges/services/baked-badge.js
+++ packages/openbadges/services/baked-badge.js
@@ -1,4 +1,10 @@
+const sharp = require("sharp");
+const pngitxt = require('png-itxt');
+const urlJoin = require('url-join');
+const { Readable } = require("stream");
+const { MIME_TYPES } = require('@semapps/mime-types');
 const { ControlledContainerMixin } = require('@semapps/ldp');
+
 module.exports = {
   name: 'openbadges.baked-badge',
   mixins: [ControlledContainerMixin],
@@ -6,5 +12,70 @@ module.exports = {
     path: '/baked-badges',
     acceptedTypes: ['semapps:File'],
     newResourcesPermissions: { anon: { read: true } },
+  },
+  actions: {
+    async bake(ctx) {
+      const { assertion, webId } = ctx.params;
+
+      const badge = await ctx.call('ldp.resource.get', {
+        resourceUri: assertion.badge,
+        accept: MIME_TYPES.JSON,
+        webId
+      });
+
+      const badgeImage = await ctx.call('ldp.resource.get', {
+        resourceUri: badge['schema:image'],
+        forceSemantic: true,
+        accept: MIME_TYPES.JSON,
+        webId
+      });
+
+      const bakedBadgeContainerUri = urlJoin(webId, 'data', 'baked-badges');
+
+      const bakedBadgeUri = await ctx.call('ldp.resource.generateId', {
+        containerUri: bakedBadgeContainerUri
+      })
+
+      const framedAssertion = await ctx.call('jsonld.frame', {
+        input: {
+          ...assertion,
+          'schema:image': {
+            '@id': bakedBadgeUri
+          }
+        },
+        frame: {
+          '@context': 'https://openbadgespec.org/v2/context.json',
+          '@id': assertion.id
+        }
+      });
+
+      const buffer = await sharp('./' + badgeImage['semapps:localPath']).png().toBuffer();
+      const readableStream = Readable.from(buffer);
+      const bakedFileStream = await readableStream.pipe(pngitxt.set({ keyword: 'openbadge', value: JSON.stringify(framedAssertion)} ));
+
+      // TODO see why we can't use ldp.container.post
+
+      const bakedBadge = await ctx.call('ldp.resource.upload', {
+        resourceUri: bakedBadgeUri,
+        file: {
+          encoding: badgeImage['semapps:encoding'],
+          mimetype: badgeImage['semapps:mimeType'],
+          readableStream: bakedFileStream,
+        }
+      });
+
+      await ctx.call('triplestore.insert', {
+        resource: `<${bakedBadgeContainerUri}> <http://www.w3.org/ns/ldp#contains> <${bakedBadgeUri}>`,
+        webId
+      });
+
+      await ctx.call('ldp.resource.create', {
+        resource: bakedBadge,
+        contentType: MIME_TYPES.JSON,
+        webId
+      });
+
+      return bakedBadgeUri;
+    }
   }
 };

312e2731fc76adb3f766e4e48e09e3ad642cf569
srosset81 <srosset81@gmail.com>
2023-04-17 19:44:03 +0200

    Improve LocationList

--- frontend/src/resources/Location/LocationList.js
+++ frontend/src/resources/Location/LocationList.js
@@ -1,66 +1,94 @@
-import React, {useEffect} from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { useHistory } from 'react-router-dom';
-import { useListContext, useTranslate, useGetIdentity, useDataProvider,linkToRecord } from 'react-admin';
-import { makeStyles, Switch, List as MUIList, ListItem, ListItemSecondaryAction, ListItemText } from '@material-ui/core';
+import { useListContext, useTranslate, useGetIdentity, useDataProvider, linkToRecord, useNotify } from 'react-admin';
+import {
+  makeStyles,
+  Switch,
+  List as MUIList,
+  ListItem,
+  ListItemAvatar,
+  ListItemSecondaryAction,
+  ListItemText,
+  Avatar
+} from '@material-ui/core';
+import PlaceIcon from '@material-ui/icons/Place';
 import List from '../../layout/List';
 
 const useStyles = makeStyles(() => ({
-  listItem: {
+  root: {
+    height: 63,
+  },
+  container: {
     backgroundColor: 'white',
     marginBottom: 8,
+    height: 63,
     boxShadow: '0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%)',
   }
 }));
 
-const AddressList = () => {
+const ListWithSwitches = () => {
   const { ids, data } = useListContext();
-  const [checkedId, setCheckedId] = React.useState("");
+  const [checkedId, setCheckedId] = useState();
   const { identity } = useGetIdentity();
   const dataProvider = useDataProvider();
   const classes = useStyles();
   const history = useHistory();
+  const notify = useNotify();
 
-    useEffect(() => {
-        setCheckedId(identity?.profileData?.["vcard:hasAddress"])
-    }, [setCheckedId, identity]);
+  useEffect(() => {
+    setCheckedId(identity?.profileData?.["vcard:hasAddress"])
+  }, [setCheckedId, identity]);
 
-  const handleToggle = (e, id) => {
+  const setHomeAddress = useCallback(id => {
+    // If click on current address, no home address
+    if (id === checkedId) id = undefined;
     setCheckedId(id);
-    dataProvider.update("vcard:hasAddress", {
-      id: identity?.profileData?.id,
-      data: {
-        ...identity?.profileData,
-        "vcard:hasAddress": id,
-      },
-      previousData: identity?.profileData
-          })
-  };
+    dataProvider
+      .update('vcard:hasAddress', {
+        id: identity?.profileData?.id,
+        data: {
+          ...identity?.profileData,
+          'vcard:hasAddress': id
+        },
+        previousData: identity?.profileData
+      })
+      .then(() => {
+        notify('app.notification.home_address_updated',  'success');
+      });
+  }, [setCheckedId, checkedId, dataProvider, identity, notify]);
 
   return (
-      <MUIList>
-        {ids.map(id => 
-            <ListItem key={id} button onClick={() => history.push(linkToRecord('/Location', id, 'edit'))} className={classes.listItem}>
-              <ListItemText primary={data[id]["vcard:given-name"]} />
-              <ListItemSecondaryAction>
-                <Switch
-                  edge="end"
-                  onChange={e => handleToggle(e, id)}
-                  checked={id == checkedId}
-                  inputProps={{ 'aria-labelledby': 'favorite-adress' }}
-                /> 
-              </ListItemSecondaryAction>
-            </ListItem>
-        )}
-   </MUIList>
+    <MUIList>
+      {ids.map(id =>
+        <ListItem key={id} button onClick={() => history.push(linkToRecord('/Location', id, 'edit'))} classes={classes}>
+          <ListItemAvatar>
+            <Avatar>
+              <PlaceIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText
+            primary={data[id]['vcard:given-name']}
+            secondary={data[id]['vcard:hasAddress']?.['vcard:given-name']}
+          />
+          <ListItemSecondaryAction>
+            <Switch
+              edge="end"
+              onChange={e => setHomeAddress(id)}
+              checked={id === checkedId}
+            />
+          </ListItemSecondaryAction>
+        </ListItem>
+      )}
+    </MUIList>
   );
-        }
+}
 
 const LocationList = (props) => {
   const translate = useTranslate();
   
   return (
     <List title={translate('app.page.addresses')} pagination={false} {...props}>
-      <AddressList/>
+      <ListWithSwitches />
     </List>
   );
 }

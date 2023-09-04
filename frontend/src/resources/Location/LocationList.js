341de4f69720921d37a225da88dd713c063bc2de
srosset81 <srosset81@gmail.com>
2023-09-04 11:14:51 +0200

    Fix Switch on LocationList

--- frontend/src/resources/Location/LocationList.js
+++ frontend/src/resources/Location/LocationList.js
@@ -1,16 +1,7 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useListContext, useTranslate, useGetIdentity, useDataProvider, useCreatePath, useNotify } from 'react-admin';
-import {
-  Switch,
-  List as MUIList,
-  ListItem,
-  ListItemButton,
-  ListItemAvatar,
-  ListItemSecondaryAction,
-  ListItemText,
-  Avatar
-} from '@mui/material';
+import { Switch, List as MUIList, ListItem, ListItemButton, ListItemAvatar, ListItemText, Avatar } from '@mui/material';
 import makeStyles from '@mui/styles/makeStyles';
 import PlaceIcon from '@mui/icons-material/Place';
 import List from '../../layout/List';
@@ -22,7 +13,7 @@ const useStyles = makeStyles(() => ({
     padding: 0,
     height: 63,
     boxShadow: '0px 2px 1px -1px rgb(0 0 0 / 20%), 0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%)',
-  }
+  },
 }));
 
 const ListWithSwitches = () => {
@@ -36,67 +27,77 @@ const ListWithSwitches = () => {
   const notify = useNotify();
 
   useEffect(() => {
-    setCheckedId(identity?.profileData?.["vcard:hasAddress"])
+    setCheckedId(identity?.profileData?.['vcard:hasAddress']);
   }, [setCheckedId, identity]);
 
-  const setHomeAddress = useCallback(id => {
-    // If click on current address, no home address
-    if (id === checkedId) id = undefined;
-    setCheckedId(id);
-    dataProvider
-      .update('vcard:hasAddress', {
-        id: identity?.profileData?.id,
-        data: {
-          ...identity?.profileData,
-          'vcard:hasAddress': id
-        },
-        previousData: identity?.profileData
-      })
-      .then(() => {
-        if (id) {
-          notify('app.notification.home_address_updated',  { type: 'success' });
-        } else {
-          notify('app.notification.home_address_deleted',  { type: 'success' });
-        }
-      });
-  }, [setCheckedId, checkedId, dataProvider, identity, notify]);
+  const setHomeAddress = useCallback(
+    (id) => {
+      // If click on current address, no home address
+      if (id === checkedId) id = undefined;
+      setCheckedId(id);
+      dataProvider
+        .update('vcard:hasAddress', {
+          id: identity?.profileData?.id,
+          data: {
+            ...identity?.profileData,
+            'vcard:hasAddress': id,
+          },
+          previousData: identity?.profileData,
+        })
+        .then(() => {
+          if (id) {
+            notify('app.notification.home_address_updated', { type: 'success' });
+          } else {
+            notify('app.notification.home_address_deleted', { type: 'success' });
+          }
+        });
+    },
+    [setCheckedId, checkedId, dataProvider, identity, notify]
+  );
 
   return (
     <MUIList>
-      {data && data.map(record =>
-        <ListItem key={record.id} className={classes.listItem}>
-          <ListItemButton onClick={() => navigate(createPath({ resource: 'Location', id: record.id, type: 'edit' }))}>
-            <ListItemAvatar>
-              <Avatar>
-                <PlaceIcon />
-              </Avatar>
-            </ListItemAvatar>
-            <ListItemText
-              primary={record['vcard:given-name']}
-              secondary={record['vcard:hasAddress']?.['vcard:given-name']}
-            />
-            <ListItemSecondaryAction>
+      {data &&
+        data.map((record) => (
+          <ListItem
+            key={record.id}
+            className={classes.listItem}
+            secondaryAction={
               <Switch
                 edge="end"
-                onChange={e => { e.preventDefault(); setHomeAddress(record.id); }}
+                onChange={(e) => {
+                  e.preventDefault();
+                  setHomeAddress(record.id);
+                }}
                 checked={record.id === checkedId}
               />
-            </ListItemSecondaryAction>
-          </ListItemButton>
-        </ListItem>
-      )}
+            }
+          >
+            <ListItemButton onClick={() => navigate(createPath({ resource: 'Location', id: record.id, type: 'edit' }))}>
+              <ListItemAvatar>
+                <Avatar>
+                  <PlaceIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText
+                primary={record['vcard:given-name']}
+                secondary={record['vcard:hasAddress']?.['vcard:given-name']}
+              />
+            </ListItemButton>
+          </ListItem>
+        ))}
     </MUIList>
   );
-}
+};
 
 const LocationList = (props) => {
   const translate = useTranslate();
-  
+
   return (
     <List title={translate('app.page.addresses')} pagination={false} perPage={1000} {...props}>
       <ListWithSwitches />
     </List>
   );
-}
+};
 
 export default LocationList;

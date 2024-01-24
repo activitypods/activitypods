005df3e3533c2b49e842d26bbcbb8de8a1620118
srosset81 <srosset81@gmail.com>
2024-01-24 10:55:27 +0100

    Improve webhook error handling

--- packages/solid-notifications/services/provider/sub-services/webhook.js
+++ packages/solid-notifications/services/provider/sub-services/webhook.js
@@ -171,27 +171,32 @@ module.exports = {
       async process(job) {
         const { channel, activity } = job.data;
 
-        const response = await fetch(channel.sendTo, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/ld+json'
-          },
-          body: JSON.stringify(activity)
-        });
+        try {
+          const response = await fetch(channel.sendTo, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/ld+json'
+            },
+            body: JSON.stringify(activity)
+          });
 
-        if (!response.ok) {
           if (response.status === 404) {
             this.logger.warn(`Webhook ${channel.sendTo} returned a 404 error, deleting it...`);
             await this.actions.delete({ resourceUri: channel.id, webId: channel.webId });
           } else {
-            this.logger.warn(`Webhook ${channel.sendTo} returned a ${response.status} error`);
-            throw new Error(`Webhook ${channel.sendTo} returned a ${response.status} error (${response.statusText})`);
+            job.progress(100);
           }
-        } else {
-          job.progress(100);
-        }
 
-        return { ok: response.ok, status: response.status, statusText: response.statusText };
+          return { ok: response.ok, status: response.status, statusText: response.statusText };
+        } catch (e) {
+          if (job.attemptsMade + 1 >= job.opts.attempts) {
+            this.logger.warn(`Webhook ${channel.sendTo} failed ${job.opts.attempts} times, deleting it...`);
+            // DO NOT DELETE YET TO IMPROVE MONITORING
+            // await this.actions.delete({ resourceUri: channel.id, webId: channel.webId });
+          }
+
+          throw new Error(`Posting to webhook ${channel.sendTo} failed. Error: (${e.message})`);
+        }
       }
     }
   },

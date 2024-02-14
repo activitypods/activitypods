0c34ab1b9b588a0bfdde28f95a449cba87b3ca4e
srosset81 <srosset81@gmail.com>
2024-02-14 17:04:22 +0100

    Add link to Mastodon, weekly meeting + improve doc

--- website/src/navigation.js
+++ website/src/navigation.js
@@ -174,6 +174,7 @@ export const footerData = {
     // { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
     // { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
+    { ariaLabel: 'Mastodon', icon: 'tabler:brand-mastodon', href: 'https://fosstodon.org/@activitypods' },
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/assemblee-virtuelle/activitypods' },
   ],
   footNote: `

998e8d2850ce6cc4b783571b626f3d88c3c9fb8a
srosset81 <srosset81@gmail.com>
2024-03-19 12:08:19 +0100

    Initial work

---
+++ packages/mastopod/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  MastopodApp: require('./app')
+};
---
+++ packages/mastopod/services/follow.js
@@ -0,0 +1,25 @@
+const { ACTIVITY_TYPES } = require('@semapps/activitypub');
+
+module.exports = {
+  name: 'mastopod.follow',
+  dependencies: ['activity-mapping'],
+  async started() {
+    await this.broker.call('activity-mapping.addMapper', {
+      match: {
+        type: ACTIVITY_TYPES.FOLLOW
+      },
+      mapping: {
+        key: 'follow',
+        title: {
+          en: `{{#if emitterProfile.vcard:given-name}}{{{emitterProfile.vcard:given-name}}}{{else}}{{{emitter.name}}}{{/if}} is now following you`,
+          fr: `{{#if emitterProfile.vcard:given-name}}{{{emitterProfile.vcard:given-name}}}{{else}}{{{emitter.name}}}{{/if}} vous suit`
+        },
+        actionName: {
+          en: 'View',
+          fr: 'Voir'
+        },
+        actionLink: `https://mastopod.org/actor/{{encodeUri activity.actor}}`
+      }
+    });
+  }
+};

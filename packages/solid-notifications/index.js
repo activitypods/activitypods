e3bddd210fb6d7b454a5226ff956211ca16b83fa
srosset81 <srosset81@gmail.com>
2024-06-26 11:01:19 +0200

    Fix wrong import

--- packages/solid-notifications/index.js
+++ packages/solid-notifications/index.js
@@ -1,5 +1,4 @@
 module.exports = {
   NotificationListenerService: require('./services/listener'),
-  NotificationProviderService: require('./services/provider'),
-  ActivitiesListenerMixin: require('../app/services/pod-handling/pod-activities-watcher')
+  NotificationProviderService: require('./services/provider')
 };

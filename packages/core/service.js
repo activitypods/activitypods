31deab5d54cd9ffa6bc9d96e043818af40e5e4de
srosset81 <srosset81@gmail.com>
2024-07-23 13:00:08 +0200

    Remove unused attachToObjectTypes setting

--- packages/core/service.js
+++ packages/core/service.js
@@ -78,11 +78,7 @@ const CoreService = {
       settings: {
         baseUri: baseUrl,
         podProvider: true,
-        queueServiceUrl,
-        like: {
-          attachToObjectTypes: [...Object.values(OBJECT_TYPES), 'pair:Skill'],
-          attachToActorTypes: Object.values(ACTOR_TYPES)
-        }
+        queueServiceUrl
       }
     });
 

7608b786e64d09321ec96b6057bccd4b7d2efa99
srosset81 <srosset81@gmail.com>
2024-08-16 18:25:42 +0200

    Remove unneeded redirection code on AuthorizePage

--- frontend/src/pages/AuthorizePage/AuthorizePage.js
+++ frontend/src/pages/AuthorizePage/AuthorizePage.js
@@ -1,34 +1,12 @@
-import React, { useEffect } from 'react';
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import React from 'react';
 import { ThemeProvider } from '@mui/material';
 import AuthorizePageView from './AuthorizePageView';
 import theme from '../../config/theme';
 
-const AuthorizePage = () => {
-  const [searchParams] = useSearchParams();
-  const redirectTo = searchParams.get('redirect');
-  const clientId = searchParams.get('client_id');
-  const navigate = useNavigate();
-
-  // If no clientId is provided, we are not connecting to an application so we can skip this screen
-  // This happens when we go through the signup process (the ProfileCreatePage redirects here)
-  useEffect(() => {
-    if (!clientId) {
-      if (redirectTo && redirectTo.startsWith('http')) {
-        window.location.href = redirectTo;
-      } else {
-        navigate(redirectTo || '/');
-      }
-    }
-  }, [navigate, clientId, redirectTo]);
-
-  if (!clientId) return null;
-
-  return (
-    <ThemeProvider theme={theme}>
-      <AuthorizePageView />
-    </ThemeProvider>
-  );
-};
+const AuthorizePage = () => (
+  <ThemeProvider theme={theme}>
+    <AuthorizePageView />
+  </ThemeProvider>
+);
 
 export default AuthorizePage;

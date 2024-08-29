61c5d27646bc18b890734ac4ff5cdc25cc4d737f
srosset81 <srosset81@gmail.com>
2024-08-29 09:43:20 +0200

    Move eslint config to another directory

--- eslint.config.js
+++ eslint.config.js
@@ -2,10 +2,10 @@ const eslintJs = require('@eslint/js');
 const prettier = require('eslint-plugin-prettier');
 const tsEslint = require('typescript-eslint');
 // const eslintJest = require('eslint-plugin-jest');
-const eslintTsRules = require('./conf/eslint-ts-rules');
-const eslintJsRules = require('./conf/eslint-js-rules');
-const eslintReactRules = require('./conf/eslint-react-rules');
-const eslintGeneralRules = require('./conf/eslint-general-rules');
+const eslintTsRules = require('./config/eslint/ts-rules');
+const eslintJsRules = require('./config/eslint/js-rules');
+const eslintReactRules = require('./config/eslint/react-rules');
+const eslintGeneralRules = require('./config/eslint/general-rules');
 const globals = require('globals');
 
 module.exports = tsEslint.config(

f56aa4865eafcb20d5f382ba09c1ebd4cc51171a
srosset81 <srosset81@gmail.com>
2025-01-10 15:14:21 +0100

    Migration to generate registries

--- pod-provider/backend/services/utils/migration.js
+++ pod-provider/backend/services/utils/migration.js
@@ -1,5 +1,6 @@
 const { MigrationService } = require('@semapps/migration');
 const CONFIG = require('../../config/config');
+const { arrayOf } = require('@semapps/ldp');
 
 module.exports = {
   name: 'migration',
@@ -13,12 +14,56 @@ module.exports = {
       const accounts = await ctx.call('auth.account.find', { query: username === '*' ? undefined : { username } });
 
       for (const { webId } of accounts) {
-        const agent = await ctx.call('auth-agent.getAgent', { webId });
-        if (agent) {
+        let authAgent = await ctx.call('auth-agent.get', { webId });
+        if (authAgent) {
           this.logger.warn(`AuthorizationAgent already exist for ${webId}, skipping...`);
         } else {
           this.logger.info(`Creating AuthorizationAgent for ${webId}`);
-          await ctx.call('auth-agent.initializeAgent', { webId });
+          await ctx.call('auth-agent.initializeResource', { webId });
+          authAgent = await ctx.call('auth-agent.get', { webId });
+        }
+
+        const appRegistrationsContainer = await ctx.call('app-registrations.list', { webId });
+        const appRegistrations = arrayOf(appRegistrationsContainer['ldp:contains']);
+
+        for (const appRegistration of appRegistrations) {
+          this.logger.info(
+            `Adding interop:registeredWith to app registration of ${appRegistration['interop:registeredAgent']} (webId ${webId})`
+          );
+
+          await ctx.call('app-registrations.patch', {
+            resourceUri: appRegistration.id,
+            triplesToAdd: [
+              triple(
+                namedNode(appRegistration.id),
+                namedNode('http://www.w3.org/ns/solid/interop#registeredWith'),
+                namedNode(authAgent.id)
+              )
+            ],
+            webId: 'system'
+          });
+        }
+      }
+    },
+    async generateRegistries(ctx) {
+      const { username } = ctx.params;
+      const accounts = await ctx.call('auth.account.find', { query: username === '*' ? undefined : { username } });
+
+      for (const { webId } of accounts) {
+        const registrySet = await ctx.call('registry-set.get', { webId });
+        if (registrySet) {
+          this.logger.warn(`RegistrySet already exist for ${webId}, skipping...`);
+        } else {
+          this.logger.info(`Creating RegistrySet for ${webId}`);
+          await ctx.call('registry-set.initializeResource', { webId });
+        }
+
+        const authRegistry = await ctx.call('auth-registry.get', { webId });
+        if (authRegistry) {
+          this.logger.warn(`AuthorizationRegistry already exist for ${webId}, skipping...`);
+        } else {
+          this.logger.info(`Creating AuthorizationRegistry for ${webId}`);
+          await ctx.call('auth-registry.initializeResource', { webId });
         }
       }
     }

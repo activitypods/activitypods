9b3aed25e13b93f327bd498db1837616e3fb4721
Laurin Weger <Laurin-W@users.noreply.github.com>
2025-03-26 09:41:31 +0100

    migration service

---
+++ pod-provider/backend/services/migration/2-2-0.js
@@ -0,0 +1,48 @@
+const urlJoin = require('url-join');
+const { MigrationService } = require('@semapps/migration');
+const CONFIG = require('../../config/config');
+
+const MIGRATION_VERSION = '2.2.0';
+
+module.exports = {
+  name: 'migration-2-2-0',
+  mixins: [MigrationService],
+  settings: {
+    baseUrl: CONFIG.BASE_URL
+  },
+  actions: {
+    async migrate(ctx) {
+      const { username } = ctx.params;
+      const accounts = await ctx.call('auth.account.find', { query: username === '*' ? undefined : { username } });
+
+      for (const { webId, username, version, ...rest } of accounts) {
+        if (version === MIGRATION_VERSION) {
+          this.logger.info(`Pod of ${webId} is already on v${MIGRATION_VERSION}, skipping...`);
+        } else {
+          this.logger.info(`Migrating Pod of ${webId} to v${MIGRATION_VERSION}...`);
+
+          ctx.meta.dataset = username;
+          ctx.meta.webId = webId;
+          ctx.meta.skipObjectsWatcher = true; // We don't want to trigger an Update
+
+          try {
+            // Delete old capabilities containers.
+            const containerUri = urlJoin(webId, 'data/capabilities');
+            await ctx.call('ldp.container.delete', { containerUri, webId: 'system' });
+
+            await ctx.call('auth.account.update', {
+              id: rest['@id'],
+              webId,
+              username,
+              version: MIGRATION_VERSION,
+              ...rest
+            });
+          } catch (e) {
+            this.logger.error(`Unable to migrate Pod of ${webId} to ${MIGRATION_VERSION}. Error: ${e.message}`);
+            console.error(e);
+          }
+        }
+      }
+    }
+  }
+};

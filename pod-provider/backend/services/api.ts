b2cf3b9b8dd8f30e49f21945c68a31c536ec2db8
srosset81 <srosset81@gmail.com>
2025-12-01 15:42:14 +0100

    Moving type-index and notifications tests to SemApps

--- pod-provider/backend/services/api.ts
+++ pod-provider/backend/services/api.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import ApiGatewayService from 'moleculer-web';
 // @ts-expect-error TS(2614): Module '"moleculer-web"' has no exported member 'E... Remove this comment to see the full error message
 import { Errors as E } from 'moleculer-web';
-import WebSocketMixin from '../mixins/websocket.ts';
+import { WebSocketMixin } from '@semapps/solid';
 import * as CONFIG from '../config/config.ts';
 import { ServiceSchema } from 'moleculer';
 
